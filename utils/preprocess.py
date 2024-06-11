2281f54333942ba18ba0c0e27a4e1c407b566d08
Vitor França <vitor.sousa.franca@gmail.com>
2024-06-10 22:33:01 -0300

    add: preprocess.py to load data and do the preprocess pipeline

---
+++ utils/preprocess.py
@@ -0,0 +1,64 @@
+import pandas as pd
+from sklearn.preprocessing import OneHotEncoder
+from sklearn.pipeline import Pipeline
+from sklearn.preprocessing import StandardScaler, MinMaxScaler
+from sklearn.impute import SimpleImputer
+from sklearn.compose import ColumnTransformer
+
+class LoadData:
+    def __init__(self):
+         # Load data
+        data = pd.read_excel('content/data_trainE_L_MARÇO_COMPLETO.xlsx')
+
+        # Rename columns
+        data.rename(columns={
+            'BOOSTING (MWH)': 'boosting',
+            'Cor': 'cor',
+            'Espess.': 'espessura',
+            'Extração forno': 'extracao_forno',
+            '%CACO': 'porcentagem_caco'
+        }, inplace=True)
+
+        # Standardize string columns to lowercase
+        self.data = data.map(lambda x: x.lower() if isinstance(x, str) else x)
+
+        # Identificar colunas categóricas e numéricas
+        self.categorical_features = ['cor']  # Colunas categóricas
+        self.numerical_features = ['boosting', 'espessura', 'extracao_forno', 'porcentagem_caco']  # Colunas numéricas
+        
+        
+        self.features = self.numerical_features + self.categorical_features
+        self.target = 'Médio diário'  # Coluna alvo
+
+
+
+    def create_preprocessor(self, scale_std=False, scale_minmax=False):
+
+        # Transformer to numerical columns
+        steps = [('imputer', SimpleImputer(strategy='mean'))] # If there are missing values, fill with the mean
+
+        # Add scaler to the pipeline
+        if scale_std and scale_minmax:
+            raise ValueError('Only one scaler can be selected')
+        elif scale_std:
+            steps.append(('scaler', StandardScaler()))
+        elif scale_minmax:
+            steps.append(('scaler', MinMaxScaler()))
+
+        numeric_transformer = Pipeline(steps=steps) # Pipeline to numerical columns
+
+        
+        # Transformer to categorical columns
+        categorical_transformer = Pipeline(steps=[
+            ('onehot', OneHotEncoder())
+        ])
+
+
+        # Preprocessador
+        preprocessor = ColumnTransformer(transformers=[
+                ('num', numeric_transformer, self.numerical_features),
+                ('cat', categorical_transformer, self.categorical_features)])
+        
+
+        return preprocessor
+
