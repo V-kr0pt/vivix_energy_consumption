3e1fc07d651d873f9bac27ea9d6a55975be62e55
Vitor França <vitor.sousa.franca@gmail.com>
2024-06-21 13:11:57 -0300

    add: KNN model first grid search

---
+++ KNN_model.py
@@ -0,0 +1,57 @@
+from sklearn.neighbors import KNeighborsRegressor as knr
+from sklearn.model_selection import train_test_split
+from utils.model_utils import Model_utils 
+from utils.preprocess import LoadData 
+
+
+# comments to be saved in the history
+comments = '4th grid search'
+
+load_data = LoadData()
+
+data = load_data.data
+
+features = load_data.features
+target = load_data.target
+
+X = data[features]
+y = data[target]
+
+# Scale is not needed for XGBoost (it is a tree-based model)
+preprocessor = load_data.create_preprocessor(scale_std=False, scale_minmax=False)
+
+# Split the data into training and test sets
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
+
+# Preprocess the data
+X_train = preprocessor.fit_transform(X_train)
+
+# Train the model
+model_name = 'KNN'
+# Define the parameter grid for grid search
+param_grid = {
+    'algorithm': ['ball_tree', 'kd_tree', 'brute'],
+    'n_neighbors': [4, 5, 6],
+    'weights': ['uniform', 'distance'],
+    'leaf_size': [1, 2, 3],
+    'p': [1]
+}
+
+# Create the KN-Regressor model
+model = knr()
+#model = xgb.XGBRegressor(objective='reg:squarederror', enable_categorical='True')
+model_utils = Model_utils()
+
+# Train the model with the best parameters
+model_utils.train_model(model, X_train, y_train, model_name, preprocessor=preprocessor, grid_search=True, param_grid=param_grid, comments=comments)
+#model_utils.train_model(model, X_train, y_train, model_name, preprocessor=preprocessor, grid_search=False, comments=comments)
+
+# Load the model with the best parameters + the preprocessor
+model, preprocessor = model_utils.load_model()
+
+# Preprocess the test data
+X_test = preprocessor.transform(X_test) 
+
+# Test the model
+y_pred = model_utils.test_model(X_test, y_test)
+
