e07a566c553c9a4e24ed2f5b30f67bddb7252a44
Vitor França <vitor.sousa.franca@gmail.com>
2024-07-16 18:51:13 -0300

    add: best xgboost model

--- xgboost_model.py
+++ xgboost_model.py
@@ -8,7 +8,7 @@ from utils.load_data import LoadData
 from utils.preprocess import Preprocess
 
 # comments to be saved in the history
-comments = '7 lag media_diario + 2 lag all features, 10 splits'
+comments = 'shuffle data + 7 lag media_diario + 2 lag all features, 3 splits'
 model_name = 'xgboost'
 
 load_data = LoadData()
@@ -26,8 +26,11 @@ lag_columns_list += load_data.features
 lag_values += [1,2] * len(load_data.features)
 
 # create the lagged columns in data
-data = preprocess.create_lag_columns(lag_columns_list, lag_values)
-data = data.iloc[7:]
+train_data = preprocess.create_lag_columns(lag_columns_list, lag_values)
+train_data = train_data.iloc[7:]
+
+# shuffling data
+#train_data = train_data.sample(frac=1).reset_index(drop=True)
 
 features = preprocess.features
 target = preprocess.target
@@ -43,16 +46,28 @@ X_train = preprocessor.fit_transform(X_train)
 
 # Train the model
 # Define the parameter grid for grid search
+#param_grid = {
+#    'n_estimators': [700, 800, 900],
+#    'max_depth': [1, 2, 3],
+#    'learning_rate': [0.01],
+#    'gamma': [0], # Minimum loss reduction required to make a further partition on a leaf node of the tree
+#    'subsample': [0.4, 0.5, 0.6],
+#    'reg_alpha': [0.1, 0.2, 0.3, 0.4, 0.5], # L1 regularization
+#    'reg_lambda': [0, 0.01], # L2 regularization
+#    'random_state': [42]
+#}
+
 param_grid = {
-    'n_estimators': [700, 800, 900],
-    'max_depth': [1, 2, 3],
+    'n_estimators': [800],
+    'max_depth': [2],
     'learning_rate': [0.01],
     'gamma': [0], # Minimum loss reduction required to make a further partition on a leaf node of the tree
-    'subsample': [0.4, 0.5, 0.6],
-    'reg_alpha': [0.1, 0.2, 0.3, 0.4, 0.5], # L1 regularization
-    'reg_lambda': [0, 0.01], # L2 regularization
+    'subsample': [0.5],
+    'reg_alpha': [0.5], # L1 regularization
+    'reg_lambda': [0.01], # L2 regularization
     'random_state': [42]
 }
+
 
 # Define the parameters
 #params = {
@@ -74,7 +89,7 @@ model = xgb.XGBRegressor() #xgb.XGBRegressor(**params)
 
 # Train the model
 # TimeSeriesSplit Config
-tscv = TimeSeriesSplit(n_splits=10)
+tscv = TimeSeriesSplit(n_splits=3)
 
 grid_search = GridSearchCV(estimator=model, param_grid=param_grid, cv=tscv, scoring='neg_mean_squared_error', n_jobs=-1)
 grid_search.fit(X_train, y_train)
