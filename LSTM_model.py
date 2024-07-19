f47ef4bb885112815d586da6cafde548a6b1eefb
Vitor França <vitor.sousa.franca@gmail.com>
2024-07-19 16:43:01 -0300

    fix: LSTM model as a torch.nn.Module

--- LSTM_model.py
+++ LSTM_model.py
@@ -1,18 +1,18 @@
 import torch
 from sklearn.base import BaseEstimator, RegressorMixin
-from utils.model_utils import Model_utils 
+from torch.utils.data import DataLoader, TensorDataset
 
 
-class LSTM_model:
+class LSTM_model(torch.nn.Module):
     def __init__(self, model_params: dict, model_name: str = "LSTM"):
-        self.model_utils = Model_utils()
+        super(LSTM_model, self).__init__()
+
         self.model = None
         self.model_name = model_name 
         self.model_params = model_params
         self.train_losses = []  # Store training losses for each epoch
-           
 
-    def create_model(self, input_size):
+        input_size = self.model_params['input_size']
         hidden_layer_size = self.model_params['hidden_layer_size']
         num_layers = self.model_params['num_layers']
         output_size = self.model_params['output_size']
@@ -22,7 +22,7 @@ class LSTM_model:
         self.model = torch.nn.LSTM(input_size=input_size,
                                     hidden_size=hidden_layer_size, 
                                     num_layers=num_layers, batch_first=True)
-        self.fc = torch.nn.Linear(hidden_layer_size, output_size) 
+        self.fc = torch.nn.Linear(hidden_layer_size, output_size)         
 
 
     def forward_pass(self, x):
@@ -39,8 +39,9 @@ class LSTM_model:
         X_train = torch.from_numpy(X_train).float()
         y_train = torch.from_numpy(y_train).float()   
         # Create Dataloader
-        train_loader = self.model_utils.create_Dataloader(X_train, y_train, batch_size)
-
+        dataset = TensorDataset(X_train, y_train)
+        train_loader = DataLoader(dataset, batch_size=batch_size, shuffle=False)
+       
         self.model.train()
         for epoch in range(epochs):
             epoch_loss = 0
@@ -73,7 +74,7 @@ class LSTM_model:
 
 # To be possible to use GridSearchCV
 class LSTMModelWrapper(BaseEstimator, RegressorMixin):
-    def __init__(self, hidden_layer_size=50, num_layers=1, output_size=1, learning_rate=0.001, epochs=10, verbose=False):
+    def __init__(self, input_size=1, hidden_layer_size=50, num_layers=1, output_size=1, learning_rate=0.001, epochs=10, verbose=False):
         self.hidden_layer_size = hidden_layer_size
         self.num_layers = num_layers
         self.learning_rate = learning_rate
@@ -84,13 +85,13 @@ class LSTMModelWrapper(BaseEstimator, RegressorMixin):
             'hidden_layer_size': hidden_layer_size,
             'num_layers': num_layers,
             'learning_rate': learning_rate,
-            'output_size': output_size
+            'output_size': output_size,
+            'input_size': input_size
         })
 
 
     def fit(self, X, y):
-        input_size = X.shape[1]
-        self.model.create_model(input_size)
+        #self.model.input_size = X.shape[1]
         self.model.fit(X, y, self.epochs, verbose=self.verbose)
         return self
 
