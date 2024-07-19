5520e1947d0a1adb82ecece2c865e239584da9d7
Vitor França <vitor.sousa.franca@gmail.com>
2024-07-19 16:42:19 -0300

    fix: removing torch dataloader + seq_length from model_params

--- utils/model_utils.py
+++ utils/model_utils.py
@@ -2,7 +2,6 @@ import pickle
 from datetime import datetime
 from sklearn.model_selection import GridSearchCV
 from sklearn.metrics import mean_absolute_error, mean_squared_error, r2_score
-from torch.utils.data import DataLoader, TensorDataset
 
 import seaborn as sns
 import matplotlib.pyplot as plt
@@ -171,15 +170,8 @@ class Model_utils:
             fig.savefig(plot_path)
 
         return plot_path
-    
-    def create_Dataloader(self, X, y, batch_size=32):
-        # Create DataLoader for batching
-        dataset = TensorDataset(X, y)
-        dataloader = DataLoader(dataset, batch_size=batch_size, shuffle=False)
-        return dataloader
 
-    def create_sequences(self, X, y):
-        seq_length = self.model_params['seq_length']
+    def create_sequences(self, X, y, seq_length):
         X_seq = []
         y_seq = []
         for i in range(len(X) - seq_length):
