871293f584828eaa3ea7b60eb3cc0c31893eeeb5
Vitor França <vitor.sousa.franca@gmail.com>
2024-08-15 13:53:57 -0300

    add: preprocess of potencias_geral data done! (load_data.py)

--- utils/load_data.py
+++ utils/load_data.py
@@ -1,39 +1,78 @@
 import pandas as pd
 
 class LoadData:
-    def __init__(self, path='content/cleaned_data_train.xlsx'):
+    def __init__(self, path='content/potencias_geral.xlsx'):
         data = pd.read_excel(path)
 
         # Rename columns
+        # all in lower case
+        data.columns = data.columns.str.lower()
+
         data.rename(columns={
-            'BOOSTING (MWH)': 'boosting',
-            'Cor': 'cor',
-            'Prod_E': 'prod_e',
-            'Prod_L': 'prod_l',
-            'Espess.': 'espessura',
-            'Extração forno': 'extracao_forno',
-            '%CACO': 'porcentagem_caco',
-            'Médio diário': 'medio_diario'
+            'ativa consumo (kwh)': 'consumo_kwh'
         }, inplace=True)
 
-        # Standardize string columns to lowercase
-        data = data.applymap(lambda x: x.lower() if isinstance(x, str) else x)
+        # create datetime column to group by and sum the consumption
+        data['datetime'] = data.apply(self.adjust_time, axis=1)
 
-        # Add week_day column
-        data['week_day'] = data['Data'].dt.dayofweek
+        # Group by datetime and ponto_de_medicao
+        data = data.groupby(['datetime'])['consumo_kwh'].sum().reset_index()
+
+        # Standardize string columns to lowercase
+        #data = data.applymap(lambda x: x.lower() if isinstance(x, str) else x)
 
         # Garantee that the data is sorted by date
-        data = data.sort_values(by='Data')
+        data = data.sort_values(by='datetime')
+
+        # now divide the data column into year, month, week_day and hour
+        data['year'] = data['datetime'].dt.year
+        data['month'] = data['datetime'].dt.month
+        data['day'] = data['datetime'].dt.day
+        data['week_day'] = data['datetime'].dt.dayofweek
+        data['hour'] = data['datetime'].dt.hour
+
+        # Now we only need the max consumation value from the day
+        data['consumo_max_diario'] = data.groupby(['year', 'month', 'day'])['consumo_kwh'].transform('max')
+        data = data[data['consumo_kwh'] == data['consumo_max_diario']]
+        data.drop(columns=['consumo_kwh'], inplace=True)
 
         # Create a different dataframe with only the last month data (last 30 days)
-        last_30_days = data['Data'].max() - pd.Timedelta(days=30)
-        self.last_month_data = data[data['Data'] >= last_30_days]
-        self.data = data[data['Data'] < last_30_days]
+        last_30_days = data['datetime'].max() - pd.Timedelta(days=30)
+        self.last_month_data = data[data['datetime'] >= last_30_days]
+        self.data = data[data['datetime'] < last_30_days]
+
+        # Finally we can drop the datetime column
+        self.data.drop(columns=['datetime'], inplace=True)
+        self.last_month_data.drop(columns=['datetime'], inplace=True)
 
         # Identify categorical and numerical columns
-        self.boolean_features = ['prod_e', 'prod_l']  # Boolean columns
-        self.categorical_features = ['cor', 'week_day']  # Categorical columns
-        self.numerical_features = ['boosting', 'espessura', 'extracao_forno', 'porcentagem_caco']  # Numerical columns
+        self.boolean_features = []  # Boolean columns
+        self.categorical_features = ['year', 'month', 'week_day', 'hour', 'day']  # Categorical columns
+        self.numerical_features = []  # Numerical columns
         
+        # create a list with all features and the target
         self.features = self.numerical_features + self.categorical_features + self.boolean_features
-        self.target = 'medio_diario'
+        self.target = 'consumo_max_diario'
+
+
+    def adjust_time(self, row):
+        if row['hora'] == 24:
+            return row['data'] + pd.Timedelta(days=1)
+        else:
+            return pd.to_datetime(f"{row['data']} {row['hora']}:00") 
+
+
+if __name__ == '__main__':
+    load_data = LoadData()
+    print('\n\t\t---- Data ----')
+    print(load_data.data.head())
+    print('shape: ', load_data.data.shape)
+    print('\n\t\t---- Last month data ----\t')
+    print(load_data.last_month_data.head())
+    print('shape: ', load_data.last_month_data.shape)
+    print('\n\t\t---- Features ----')
+    print(load_data.features)
+    print('number of features: ', len(load_data.features))
+    print('\n\t\t---- Target ----')
+    print(load_data.target)
+    print('\n')
