30696a09ac3fa49392d1fd7c9b0de8040b2d9d17
Vitor França <vitor.sousa.franca@gmail.com>
2024-08-15 18:15:26 -0300

    add: old_data features with new max column

--- utils/load_data.py
+++ utils/load_data.py
@@ -3,6 +3,7 @@ import pandas as pd
 class LoadData:
     def __init__(self, path='content/potencias_geral.xlsx'):
         data = pd.read_excel(path)
+        old_data = pd.read_excel('content/cleaned_data_train.xlsx')
 
         # Rename columns
         # all in lower case
@@ -10,7 +11,20 @@ class LoadData:
 
         # transform kWh to MWh
         data['ativa consumo (kwh)'] = data['ativa consumo (kwh)'] / 1000
-        data.rename(columns={'ativa consumo (kwh)': 'consumo_mwh'}, inplace=True)        
+        data.rename(columns={'ativa consumo (kwh)': 'consumo_mwh'}, inplace=True) 
+
+        # Rename columns
+        old_data.rename(columns={
+            'BOOSTING (MWH)': 'boosting',
+            'Cor': 'cor',
+            'Prod_E': 'prod_e',
+            'Prod_L': 'prod_l',
+            'Espess.': 'espessura',
+            'Extração forno': 'extracao_forno',
+            '%CACO': 'porcentagem_caco',
+            'Médio diário': 'medio_diario',
+            'Data':'datetime'
+        }, inplace=True)       
 
         # create datetime column to group by and sum the consumption
         data['datetime'] = data.apply(self.adjust_time, axis=1)
@@ -19,7 +33,7 @@ class LoadData:
         data = data.groupby(['datetime'])['consumo_mwh'].sum().reset_index()
 
         # Standardize string columns to lowercase
-        #data = data.applymap(lambda x: x.lower() if isinstance(x, str) else x)
+        data = data.applymap(lambda x: x.lower() if isinstance(x, str) else x)
 
         # Garantee that the data is sorted by date
         data = data.sort_values(by='datetime')
@@ -36,6 +50,15 @@ class LoadData:
         data = data[data['consumo_mwh'] == data['consumo_max_diario']]
         data.drop(columns=['consumo_mwh'], inplace=True)
 
+        # we can concatenate the old data with the new one
+        # unfortunnaly we will losing some data but we have more features
+        print('Data lost: ', data.shape[0] - old_data.shape[0])
+        data['datetime'] = data['datetime'].dt.date
+        old_data['datetime'] = old_data['datetime'].dt.date
+        data = pd.merge(data, old_data, on='datetime')
+        # the medio_diario is not useful anymore
+        data.drop(columns=['medio_diario'], inplace=True)
+
         # Create a different dataframe with only the last month data (last 30 days)
         last_30_days = data['datetime'].max() - pd.Timedelta(days=30)
         self.last_month_data = data.loc[data['datetime'] >= last_30_days].copy()
@@ -46,9 +69,9 @@ class LoadData:
         self.last_month_data.drop(columns=['datetime'], inplace=True)
 
         # Identify categorical and numerical columns
-        self.boolean_features = []  # Boolean columns
-        self.categorical_features = ['year', 'month', 'week_day', 'hour', 'day']  # Categorical columns
-        self.numerical_features = []  # Numerical columns
+        self.boolean_features = ['prod_e', 'prod_l'] # Boolean columns
+        self.categorical_features = ['year', 'month', 'week_day', 'hour', 'day', 'cor', 'week_day']  # Categorical columns
+        self.numerical_features = ['boosting', 'espessura', 'extracao_forno', 'porcentagem_caco']   # Numerical columns
         
         # create a list with all features and the target
         self.features = self.numerical_features + self.categorical_features + self.boolean_features
